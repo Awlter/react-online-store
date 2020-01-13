7221b4738302ee2071fc5280b059c0d9647ce9b2
wangxuefei <xuefeiwang@jinshuju.net>
2020-01-13 22:40:50 +0800

    layout style

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,42 +1,44 @@
 import React from "react";
 
+import "./homepage.styles.scss";
+
 const Homepage = () => {
   return (
     <div>
       <div className="homepage">
-        <div className="menu-directory">
+        <div className="directory-menu">
           <div className="menu-item">
             <div className="content">
-              <h1>HATS</h1>
-              <span>SHOP NOW</span>
+              <h1 className="title">HATS</h1>
+              <span className="subtitle">SHOP NOW</span>
             </div>
           </div>
 
           <div className="menu-item">
             <div className="content">
-              <h1>JACKETS</h1>
-              <span>SHOP NOW</span>
+              <h1 className="title">HATS</h1>
+              <span className="subtitle">SHOP NOW</span>
             </div>
           </div>
 
           <div className="menu-item">
             <div className="content">
-              <h1>SNEAKERS</h1>
-              <span>SHOP NOW</span>
+              <h1 className="title">HATS</h1>
+              <span className="subtitle">SHOP NOW</span>
             </div>
           </div>
 
           <div className="menu-item">
             <div className="content">
-              <h1>WOMENS</h1>
-              <span>SHOP NOW</span>
+              <h1 className="title">HATS</h1>
+              <span className="subtitle">SHOP NOW</span>
             </div>
           </div>
 
           <div className="menu-item">
             <div className="content">
-              <h1>MENS</h1>
-              <span>SHOP NOW</span>
+              <h1 className="title">HATS</h1>
+              <span className="subtitle">SHOP NOW</span>
             </div>
           </div>
         </div>
