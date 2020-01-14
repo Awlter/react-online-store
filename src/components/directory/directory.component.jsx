cbf34c0950680d2b934bfb9127a3779cf44909a0
wangxuefei <xuefeiwang@jinshuju.net>
2020-01-14 17:12:21 +0800

    sections bg

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -45,8 +45,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ id, title }) => (
-          <MenuItem key={id} title={title} />
+        {this.state.sections.map(({ id, title, imageUrl, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
         ))}
       </div>
     );
