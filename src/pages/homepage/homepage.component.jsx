0b3bbd676a1316688a7d7a9aaa4b7e5b58f3eacc
wangxuefei <xuefeiwang@jinshuju.net>
2020-01-14 17:04:41 +0800

    split different components into different directories

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./homepage.styles.scss";
+import Directory from "../../components/directory/directory.component";
+
+const Homepage = () => {
+  return (
+    <div>
+      <div className="homepage">
+        <Directory />
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
