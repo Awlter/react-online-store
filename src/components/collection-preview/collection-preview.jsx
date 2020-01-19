983ae0d61349ca12ed4cfdf8108cd062d51af0c1
wangxuefei <xuefeiwang@jinshuju.net>
2020-01-19 22:19:00 +0800

    add collection item component

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./collection-preview.scss";
+import CollectionItem from "../collection-item/collection-item.component";
 
 const CollectionPreview = ({ title, items }) => {
   return (
@@ -8,8 +9,8 @@ const CollectionPreview = ({ title, items }) => {
       <div className="preview">
         {items
           .filter((_, indx) => indx < 4)
-          .map(item => (
-            <div key={item.id}>{item.name}</div>
+          .map(({ id, ...otherProps }) => (
+            <CollectionItem key={id} {...otherProps} />
           ))}
       </div>
     </div>
