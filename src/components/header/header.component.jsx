f01ea3b131781b82ff55d1200023938b9826c1f4
wangxuefei <xuefeiwang@jinshuju.net>
2020-01-19 22:35:45 +0800

    add header component and links to home, shop page and contact

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+import "./header.styles.scss";
+
+const Header = () => (
+  <div className="header">
+    <div className="logo-container">
+      <Link className="logo" to="/">
+        <Logo />
+      </Link>
+    </div>
+    <div className="options">
+      <Link className="option" to="shop">
+        Shop
+      </Link>
+      <Link className="option" to="contacts">
+        Contacts
+      </Link>
+    </div>
+  </div>
+);
+
+export default Header;
