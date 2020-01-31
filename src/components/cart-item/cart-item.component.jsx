b00cb94168ae9789d36e782218017e59affb7fee
wangxuefei <xuefeiwang@jinshuju.net>
2020-01-31 23:34:09 +0800

    another layout version for cart dropdown items using flex

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -3,15 +3,12 @@ import "./cart-item.styles.scss";
 const CartItem = ({ imageUrl, name, price, itemCount }) => {
   return (
     <div className="cart-item">
-      <div
-        className="image"
-        style={{ backgroundImage: `url(${imageUrl})` }}
-      ></div>
-      <div className="name">{name}</div>
-      <div className="info">
-        <span>{itemCount}</span>
-        <span>x</span>
-        <span>${price}</span>
+      <img src={imageUrl} alt="item" />
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">
+          {itemCount} x ${price}
+        </span>
       </div>
     </div>
   );
